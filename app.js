746bb83f6490f195cd2c9bd140a85011d52f7d67
alvaro893 <alvarobolro@gmail.com>
2017-01-26 16:14:42 +0200

    add security password and refactor the app

--- app.js
+++ app.js
@@ -3,81 +3,79 @@ const url = require('url')
 var port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-const PASSWORD = "90011"
+const PASSWORD = process.env.WS_PASSWORD
 const camDataPath = "/camera"
 const clientDataPath = "/client"
 var camSocket = null
 var clientSocket = null
 
 const wss = new WebSocket.Server({
-     host: ip, port: port, verifyClient: verifyClient});
+    host: ip,
+    port: port,
+    verifyClient: verifyClient
+});
 
-var buffer = "data"
 console.log("running on %s:%d", wss.options.host, wss.options.port)
 
 wss.on('connection', function connection(ws) {
     var parsedUrl = url.parse(ws.upgradeReq.url)
     var path = parsedUrl.pathname
+    var incoming = null
 
-    if(path == camDataPath){
-        camSocket = ws
-        var incoming = function incoming(message, flags) {
-            console.log('received from cam: %d', message.length);
-            if (clientSocket != null){
-                try{
-                clientSocket.send(message)
-                }catch (e){
-                    console.error(e)
-                }
-            }
-        }
-    }else if (path == clientDataPath){
-        clientSocket = ws
-        var incoming = function incoming(message, flags) {
-            console.log('received from client: %s', message);
-                if (camSocket != null){
-                try{
-                camSocket.send(message)
-                }catch (e){
-                    console.error(e)
-                }
-            }
-        }
-    }else if(path == "/"){
-        // this is a copy of client path
-        clientSocket = ws
-        var incoming = function incoming(message) {
-            console.log('received from client: %s', message);
-                if (camSocket != null){
-                try{
-                camSocket.send(message)
-                }catch (e){
-                    console.error(e)
-                }
-            }
-        }
-    }else{
-        console.warn("rejected: no valid path");
-        ws.terminate()
-        return
+    switch (path) {
+        case camDataPath:
+            camSocket = ws
+            incoming = incomingFromClient;
+            break;
+        case clientDataPath:
+        case "/":
+            clientSocket = ws
+            incoming = incomingFromCamera;
+            break;
+        default:
+            console.warn("rejected: no valid path");
+            ws.terminate()
+            return
     }
-        
-  ws.on('message', incoming);
+
+    ws.on('message', incoming);
+    ws.on('close', function(code, message) {
+        console.log("client close connection: %d, %s", code, message)
+    })
 });
 
-function verifyClient(info){
+function incomingFromCamera(message, flags) {
+    if (camSocket != null) {
+        try {
+            camSocket.send(message)
+        } catch (e) {
+            console.error(e)
+        }
+    }
+}
+
+function incomingFromClient(message, flags) {
+    if (clientSocket != null) {
+        try {
+            clientSocket.send(message)
+        } catch (e) {
+            console.error(e)
+        }
+    }
+}
+
+function verifyClient(info) {
     var acceptHandshake = false
     var accepted = "not accepted"
 
     clientUrl = url.parse(info.req.url, true)
     params = clientUrl.query
-    
-    acceptHandshake = true//params.pass == PASSWORD
 
-    if(acceptHandshake){
+    acceptHandshake = params.pass == PASSWORD
+
+    if (acceptHandshake) {
         accepted = "accepted"
     }
     console.warn("new client %s: %s", accepted, info.origin)
     return acceptHandshake
 }
-
