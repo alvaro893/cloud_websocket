78f7d1b3114450d6896c62a0c832f934bc61c5a1
alvaro893 <alvarobolro@gmail.com>
2017-01-31 01:43:06 +0200

    add camera connection class

--- websocketConnections.js
+++ websocketConnections.js
@@ -1,37 +1,63 @@
-
-/** class ClientConnections*/
+/**@constructor */
 function ClientConnections() {
     this.clients = []
 }
 
-ClientConnections.prototype.add = function (conn) {
+ClientConnections.prototype.add = function(conn) {
     this.clients.push(conn)
-    // console.log("client connections:%d", this.clients.length)
+        // console.log("client connections:%d", this.clients.length)
 }
 
-ClientConnections.prototype.close = function (conn) {
+ClientConnections.prototype.close = function(conn) {
     var indx = this.clients.indexOf(conn)
     this.clients.splice(indx, 1)
 }
 
-ClientConnections.prototype.incomingCallback = function (cameraConn) {
+ClientConnections.prototype.incomingCallback = function(cameraConn) {
     return function incoming(message, flags) {
         // console.log("Incoming from a client to camera"  )
-        if(cameraConn != null){
-             try {
-            cameraConn.send(message)
-        } catch (e) {
-            console.error(e)
+        if (cameraConn != null) {
+            try {
+                cameraConn.send(message)
+            } catch (e) {
+                console.error(e)
+            }
         }
-        } 
     }
 }
 
-ClientConnections.prototype.sendToAll = function (message) {
-    this.clients.forEach(function (client, ind, arr) {
+ClientConnections.prototype.sendToAll = function(message) {
+    this.clients.forEach(function(client, ind, arr) {
         client.send(message);
         // console.log("sent to " + ind)
     });
 }
+/**@constructor */
+function CameraConnections() {
+    this.cameras = []
+}
+
+CameraConnections.prototype.nConnections = function(){
+    return ""+this.cameras.length
+}
+
+CameraConnections.prototype.add = function(conn) {
+    this.cameras.push(conn)
+}
+
+CameraConnections.prototype.close = function(conn){
+    var indx = this.cameras.indexOf(conn)
+    this.clients.splice(indx, 1)
+}
+
+CameraConnections.prototype.getCamera = function(index) {
+    try {
+        return this.cameras[index]
+    } catch (e) {
+        log.error("no such camera")
+        return false
+    }
+}
 
 exports.ClientConnections = ClientConnections
+exports.CameraConnections = CameraConnections
